2b553a69479961dd8cd9d9a8938afb0d64e37016
Michael Dunleavy <mike@dunleavy.ie>
2024-12-02 13:25:56 +0000

    Plot linear and logarithmic calibration curves

--- calibrationPlotting.m
+++ calibrationPlotting.m
@@ -8,6 +8,7 @@ clear; clc; close all;
 % CHANGE TO WHEREVER YOU'VE STORED THE SPREADSHEET
 dataPath = "~/tcd/onedrive/materials/elec-lab/calibration-results.xlsx";
 
+
 % read spreadsheet
 spreadsheet = readtable(dataPath);
 
@@ -15,10 +16,16 @@ data = spreadsheet(:, 1:4);
 res = data{:, 2};
 temp = data{:, 4};
 
-constants = spreadsheet{1:3, 7};
+constants = spreadsheet{2:10, 7};
 beta = constants(1);
 refRes = constants(2);
 refTemp = constants(3);
+linearSlope = constants(4);
+linearIntercept = constants(5);
+linearRSq = constants(6);
+logSlope = constants(7);
+logIntercept = constants(8);
+logRSq = constants(9);
 
 % beta parameter function
 refConst = refRes * exp(-beta/refTemp);
@@ -32,32 +39,43 @@ ssr = sum(resisual.^2);                 % sum of square residuals
 sst = sum(deviation.^2);                % sum of square totals
 rsq = 1 - ssr/sst;
 
-% setup figure
-name = "Thermistor Calibration Curve";
-fig = figure("Name", name);
-title(name);
-xlabel("R = Resistance (\Omega)");
-ylabel("T = Temperature (K)");
-fontsize(14, "points");
+% common figure values
+xlabelVal = "R = Resistance (\Omega)";
+ylabelVal = "T = Temperature (K)";
+fontSizeVal = 14;
+markerSizeVal = 10;
+lineWidthVal = 2;
+figWidthVal = 1000;
+figHeightVal = 800;
+fitMinVal = 3000;
+fitMaxVal = 11000;
+
+
+% setup new figure
+fig = figure("Name", "Beta-parameter Thermistor Calibration Curve");
+title("\beta-parameter Thermistor Calibration Curve");
+xlabel(xlabelVal);
+ylabel(ylabelVal);
+fontsize(fontSizeVal, "points");
 hold on;
 
 % plot calibration datapoints
 obsPlot = scatter(res, temp);
-obsPlot.SizeData = 10;
+obsPlot.SizeData = markerSizeVal;
 obsPlot.MarkerFaceColor = "flat";
 hold on;
 
 % plot beta parameter trendline
-x = linspace(3000,11000);
+x = linspace(fitMinVal, fitMaxVal);
 y = arrayfun(resToTemp, x);
 betaPlot = plot(x, y);
-betaPlot.LineWidth = 2;
+betaPlot.LineWidth = lineWidthVal;
 hold on;
 
-% display line fit equation and r-squared
+% display fit equation and r-squared
 % a horrid combo of latex math and printf
 text(3600, 327, ...
-    sprintf("\\(T = \\frac{%.1f}{ln(\\frac{R}{%.5f})},\\ " + ...
+    sprintf("\\(T = \\frac{%.0f}{ln(\\frac{R}{%.5f})},\\ " + ...
         "r^2 = %.3f\\)", ...
         beta, refConst, rsq), ...
     "Interpreter", "latex", ...
@@ -66,4 +84,78 @@ text(3600, 327, ...
 % config figure
 grid; grid minor;
 legend("Observed values", "\beta-parameter fit");
-fig.Position(3:4) = [1200, 800];                    % figure size
+fig.Position(3:4) = [figWidthVal, figHeightVal];
+
+
+% setup new figure
+name = "Linear Thermistor Calibration Curve";
+fig = figure("Name", name);
+title(name);
+xlabel(xlabelVal);
+ylabel(ylabelVal);
+fontsize(fontSizeVal, "points");
+hold on;
+
+% plot calibration datapoints
+obsPlot = scatter(res, temp);
+obsPlot.SizeData = markerSizeVal;
+obsPlot.MarkerFaceColor = "flat";
+hold on;
+
+% plot linear trendline
+x = [fitMinVal, fitMaxVal];
+y = arrayfun(@(x) linearSlope*x + linearIntercept, x);
+betaPlot = plot(x, y);
+betaPlot.LineWidth = lineWidthVal;
+hold on;
+
+% display fit equation and r-squared
+% a horrid combo of latex math and printf
+text(4250, 325, ...
+    sprintf("\\(T = %.3f{\\times}10^{-3}\\,R + %.1f\\)" + "\n" + ...
+        "\\(r^2 = %.3f\\)", ...
+        linearSlope*10^3, linearIntercept, linearRSq), ...
+    "Interpreter", "latex", ...
+    "FontSize", 16);
+
+% config figure
+grid; grid minor;
+legend("Observed values", "Linear fit");
+fig.Position(3:4) = [figWidthVal, figHeightVal];
+
+
+% setup new figure
+name = "Logartihmic Thermistor Calibration Curve";
+fig = figure("Name", name);
+title(name);
+xlabel(xlabelVal);
+ylabel(ylabelVal);
+fontsize(fontSizeVal, "points");
+hold on;
+
+% plot calibration datapoints
+obsPlot = scatter(res, temp);
+obsPlot.SizeData = markerSizeVal;
+obsPlot.MarkerFaceColor = "flat";
+hold on;
+
+% plot logarithmic trendline
+x = linspace(fitMinVal, fitMaxVal);
+y = arrayfun(@(x) logSlope*log(x) + logIntercept, x);
+betaPlot = plot(x, y);
+betaPlot.LineWidth = lineWidthVal;
+hold on;
+
+% display fit equation and r-squared
+% a horrid combo of latex math and printf
+text(4250, 325, ...
+    sprintf("\\(T = %.2f\\ln(R) + %.1f\\)" + "\n" + ...
+        "\\(r^2 = %.3f\\)", ...
+        logSlope, logIntercept, logRSq), ...
+    "Interpreter", "latex", ...
+    "FontSize", 16);
+
+% config figure
+grid; grid minor;
+legend("Observed values", "Logarithmic fit");
+fig.Position(3:4) = [figWidthVal, figHeightVal];
